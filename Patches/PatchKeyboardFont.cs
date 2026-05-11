164d2f127e49c3ec64db554b946d8f8d7bbf07c7
chaixshot <chai5142xshot@gmail.com>
2026-05-11 12:37:05 +0700

    Added Keyboard font patcher

---
+++ Patches/PatchKeyboardFont.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using TMPro;
+using UnityEngine;
+using WebSocketSharp;
+using XSOverlay;
+
+namespace xsoverlay_font_changer.Patches
+{
+    [HarmonyPatch(typeof(Overlay_Manager))]
+    internal class PatchKeyboardFont
+    {
+        private static bool isPatched = false;
+
+        [HarmonyPatch("Awake")]
+        [HarmonyPostfix]
+        public static void Awake()
+        {
+            Plugin.Logger.LogInfo($"Keyboard font patcher loaded");
+        }
+
+        [HarmonyPatch(nameof(Overlay_Manager.EnableKeyboard))]
+        [HarmonyPostfix]
+        public static void Patch()
+        {
+            if (isPatched) return;
+
+            KeyboardGlobalManager keyboardManager = Plugin.overlayManager.Keyboard_Overlay.gameObject.GetComponentInChildren<KeyboardGlobalManager>(true);
+
+            if (!keyboardManager != null && keyboardManager.HasKeyboardBeenOpened)
+            {
+                Plugin.configData.TryGetValue("KeyboardFontPath", out string KeyboardFontPath);
+
+                if (KeyboardFontPath.IsNullOrEmpty())
+                {
+                    Plugin.Logger.LogError($"Config KeyboardFontPath is missing. Fallback to default");
+                    return;
+                }
+
+                Font font = new(KeyboardFontPath.Trim('"'));
+                TMP_FontAsset fontAsset = TMP_FontAsset.CreateFontAsset(font);
+
+                foreach (TextMeshProUGUI textMesh in Plugin.overlayManager.Keyboard.GetComponentsInChildren<TextMeshProUGUI>(true))
+                {
+                    textMesh.font = fontAsset;
+                }
+
+                Plugin.Logger.LogInfo($"{KeyboardFontPath}");
+                isPatched = true;
+            }
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,12 +1,10 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using TMPro;
-using UnityEngine;
 using XSOverlay;
-using XSOverlay.WebApp;
 
 namespace xsoverlay_font_changer;
 
@@ -15,22 +13,16 @@ public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
 
-    private static Overlay_Manager overlayManager;
-
-    private static Dictionary<string, string> configData;
-    private readonly Dictionary<string, bool> hasInitialized = new(){
-        { "Keyboard", false },
-    };
+    private static readonly Harmony harmony = new(MyPluginInfo.PLUGIN_GUID);
+    internal static Overlay_Manager overlayManager;
+    internal static Dictionary<string, string> configData;
 
     private void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
 
-        configData = File.ReadAllLines(@".\BepInEx\config\xsoverlay_font_changer.cfg")
-            .Where(line => !string.IsNullOrWhiteSpace(line) && !line.StartsWith("#"))
-            .Select(line => line.Split('=', (char)2))
-            .ToDictionary(parts => parts[0].Trim(), parts => parts[1].Trim());
+        harmony.PatchAll(typeof(Patches.PatchKeyboardFont));
 
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
@@ -38,26 +30,11 @@ public class Plugin : BaseUnityPlugin
     private void Start()
     {
         overlayManager = Overlay_Manager.Instance;
+        configData = File.ReadAllLines(@".\BepInEx\config\xsoverlay_font_changer.cfg")
+            .Where(line => !string.IsNullOrWhiteSpace(line) && !line.StartsWith("#"))
+            .Select(line => line.Split('=', (char)2))
+            .ToDictionary(parts => parts[0].Trim(), parts => parts[1].Trim());
 
-        ServerClientBridge.Instance.Api.Commands["Keyboard"] += delegate
-        {
-            InitializeKeyboard();
-        };
-    }
-
-    //?? InitializeKeyboard
-    private void InitializeKeyboard()
-    {
-        if (hasInitialized["Keyboard"]) return;
-        hasInitialized["Keyboard"] = true;
-
-        Logger.LogInfo("InitializeKeyboard");
-        Font font = new((configData.ContainsKey("KeyboardFontPath") ? configData["KeyboardFontPath"].Trim('"') : ""));
-        TMP_FontAsset fontAsset = TMP_FontAsset.CreateFontAsset(font);
-
-        foreach (TextMeshProUGUI textMesh in overlayManager.Keyboard.GetComponentsInChildren<TextMeshProUGUI>(true))
-        {
-            textMesh.font = fontAsset;
-        }
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is started!");
     }
 }
