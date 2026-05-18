af6c13128e1741658f34f04d02db3fc5353b9b1c
chaixshot <chai5142xshot@gmail.com>
2026-05-18 14:31:48 +0700

    Improved: patch WebView font scale changing bouncing delay

--- Patches/WebView/PatchWebView.cs
+++ Patches/WebView/PatchWebView.cs
@@ -1,6 +1,5 @@
 ﻿using BepInEx.Configuration;
 using HarmonyLib;
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using XSOverlay;
@@ -11,17 +10,6 @@ namespace xsoverlay_font_changer.Patches.WebView
     [HarmonyPatch(typeof(Overlay_Manager))]
     internal class PatchWebView
     {
-        private class ScaleDebouncer { public CancellationTokenSource Cts = new(); }
-        private static readonly ScaleDebouncer _wristDeb = new();
-        private static readonly ScaleDebouncer _wristClockDeb = new();
-        private static readonly ScaleDebouncer _wristDateDeb = new();
-        private static readonly ScaleDebouncer _wristTimeInVrDeb = new();
-        private static readonly ScaleDebouncer _settingsDeb = new();
-        private static readonly ScaleDebouncer _windowSettingsDeb = new();
-        private static readonly ScaleDebouncer _tooltipDeb = new();
-        private static readonly ScaleDebouncer _notificationDeb = new();
-        private static readonly TimeSpan _settingsScaleDeb = TimeSpan.FromMilliseconds(250);
-
         [HarmonyPatch("Awake")]
         [HarmonyPostfix]
         public static void Awake()
@@ -45,33 +33,33 @@ namespace xsoverlay_font_changer.Patches.WebView
                     break;
 
                 case OverlayWebView.UserInterfacePaths.Wrist:
-                    RegisterPatch(_wv, XConfig.WristEnable, XConfig.WristPath, XConfig.WristScale, ".media-widget-track, .media-widget-artist, .performance-bar-text-name, .performance-bar-text-extra, .performance-bar-text-percentage", _wristDeb);
-                    RegisterPatch(_wv, XConfig.WristEnable, XConfig.WristPath, XConfig.WristScale, ".clock", _wristClockDeb);
-                    RegisterPatch(_wv, XConfig.WristEnable, XConfig.WristPath, XConfig.WristScale, ".date", _wristDateDeb);
-                    RegisterPatch(_wv, XConfig.WristEnable, XConfig.WristPath, XConfig.WristScale, ".time-in-vr", _wristTimeInVrDeb);
+                    RegisterPatch(_wv, XConfig.WristEnable, XConfig.WristPath, XConfig.WristScale, ".media-widget-track, .media-widget-artist, .performance-bar-text-name, .performance-bar-text-extra, .performance-bar-text-percentage");
+                    RegisterPatch(_wv, XConfig.WristEnable, XConfig.WristPath, XConfig.WristScale, ".clock");
+                    RegisterPatch(_wv, XConfig.WristEnable, XConfig.WristPath, XConfig.WristScale, ".date");
+                    RegisterPatch(_wv, XConfig.WristEnable, XConfig.WristPath, XConfig.WristScale, ".time-in-vr");
                     break;
                 // Note: The wrist overlay has separate font size settings for the clock, date, and time-in-VR elements to ensure they remain legible and appropriately sized regardless of the overall wrist font scale.
                 // This is necessary because these elements are often smaller and more detailed than other wrist UI components, so they require different scaling to maintain readability and visual consistency.
                 //
                 case OverlayWebView.UserInterfacePaths.Settings:
-                    RegisterPatch(_wv, XConfig.SettingsEnable, XConfig.SettingsPath, XConfig.SettingsScale, ".side-bar-button-text, .page-container, .page-header-text, .page-section-text, .whitespace-pre, .description-text, .button-font-text-basic, .option, .slider-tooltips", _settingsDeb);
+                    RegisterPatch(_wv, XConfig.SettingsEnable, XConfig.SettingsPath, XConfig.SettingsScale, ".side-bar-button-text, .page-container, .page-header-text, .page-section-text, .whitespace-pre, .description-text, .button-font-text-basic, .option, .slider-tooltips");
                     break;
 
                 case OverlayWebView.UserInterfacePaths.WindowSettings:
-                    RegisterPatch(_wv, XConfig.WindowSettingsEnable, XConfig.WindowSettingsPath, XConfig.WindowSettingsScale, ".whitespace-pre, .option, .item-list-header, .item-list-appName, .item-list-title", _windowSettingsDeb);
+                    RegisterPatch(_wv, XConfig.WindowSettingsEnable, XConfig.WindowSettingsPath, XConfig.WindowSettingsScale, ".whitespace-pre, .option, .item-list-header, .item-list-appName, .item-list-title");
                     break;
 
                 case OverlayWebView.UserInterfacePaths.Tooltip:
-                    RegisterPatch(_wv, XConfig.TooltipEnable, XConfig.TooltipPath, XConfig.TooltipScale, ".tooltip-text", _tooltipDeb);
+                    RegisterPatch(_wv, XConfig.TooltipEnable, XConfig.TooltipPath, XConfig.TooltipScale, ".tooltip-text");
                     break;
 
                 case OverlayWebView.UserInterfacePaths.Notification:
-                    RegisterPatch(_wv, XConfig.NotificationEnable, XConfig.NotificationPath, XConfig.NotificationScale, "*", _notificationDeb);
+                    RegisterPatch(_wv, XConfig.NotificationEnable, XConfig.NotificationPath, XConfig.NotificationScale, "*");
                     break;
             }
         }
 
-        private static void RegisterPatch(OverlayWebView wv, ConfigEntry<bool> enable, ConfigEntry<string> path, ConfigEntry<int> scale, string cssClasses, ScaleDebouncer debouncer)
+        private static void RegisterPatch(OverlayWebView wv, ConfigEntry<bool> enable, ConfigEntry<string> path, ConfigEntry<int> scale, string cssClasses)
         {
             // Get scale adjustment based on CSS class to ensure consistent visual size across different UI elements
             static int GetScaleAdjustment(string css) =>
@@ -82,6 +70,7 @@ namespace xsoverlay_font_changer.Patches.WebView
                     ".time-in-vr" => 2,
                     _ => 0
                 };
+
 
             // Apply font style if enabled on initial load
             if (enable.Value)
@@ -108,15 +97,18 @@ namespace xsoverlay_font_changer.Patches.WebView
             };
 
             //Change font scale with debouncing to prevent excessive updates
+            CancellationTokenSource debounceCts = null;
             scale.SettingChanged += async (s, e) =>
             {
-                var previous = Interlocked.Exchange(ref debouncer.Cts, new CancellationTokenSource());
-                previous?.Cancel();
+                if (enable.Value)
+                {
+                    debounceCts?.Cancel();
+                    debounceCts?.Dispose();
+                    debounceCts = new CancellationTokenSource();
 
-                var ct = debouncer.Cts.Token;
-                await Task.Delay(_settingsScaleDeb, ct).ConfigureAwait(false);
-                if (ct.IsCancellationRequested) return;
-                Execute.ApplyHtmlStyle(wv, path.Value, scale.Value + GetScaleAdjustment(cssClasses), cssClasses);
+                    await Task.Delay(500, debounceCts.Token);
+                    Execute.ApplyHtmlStyle(wv, path.Value, scale.Value + GetScaleAdjustment(cssClasses), cssClasses);
+                }
             };
         }
     }
