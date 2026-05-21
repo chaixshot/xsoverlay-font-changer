1f0caf79dc473376293d5cbd3efab20f63f7e355
chaixshot <chai5142xshot@gmail.com>
2026-05-21 11:53:14 +0700

    pump version 1.1.0

--- MyPluginInfo.cs
+++ MyPluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "xsoverlay.font.changer";
         public const string PLUGIN_NAME = "XSOverlay Font Changer";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.1.0";
     }
 }
