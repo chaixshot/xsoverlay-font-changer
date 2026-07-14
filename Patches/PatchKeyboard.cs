7fafec3028320d5dc4147c0c19f276ff5a741ea9
chaixshot <chai5142xshot@gmail.com>
2026-07-14 22:00:28 +0700

    Changes: rework PatchKeyboard

--- Patches/PatchKeyboard.cs
+++ Patches/PatchKeyboard.cs
@@ -4,6 +4,7 @@ using HarmonyLib;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Reflection.Emit;
 using TMPro;
 using UnityEngine;
 using XSOverlay;
@@ -14,7 +15,15 @@ namespace xsoverlay_font_changer.Patches
     {
         private static bool IsKeyboardExist = false;
         private static Overlay_Manager KeyboardOverlay;
+        private static TMP_FontAsset CachedFontAsset;
         private static readonly Dictionary<int, (TMP_FontAsset fontAsset, float fontSize)> Original = [];
+
+        private static readonly FieldInfo DisplayTextFontScale = AccessTools.Field(typeof(KeyboardKey), "DisplayTextFontScale");
+        private static readonly FieldInfo CornerTextFontScale = AccessTools.Field(typeof(KeyboardKey), "CornerTextFontScale");
+        private static readonly FieldInfo MainText = AccessTools.Field(typeof(KeyboardKey), "MainText");
+
+        private static float GetMainTextSize() => 24f + (IsEnabled() ? XConfig.KeyboardScale.Value : 0f);
+        private static float GetSecondaryTextSize() => 40f + (IsEnabled() ? XConfig.KeyboardScale.Value : 0f);
 
         [HarmonyPatch(typeof(Overlay_Manager), "Awake")]
         [HarmonyPostfix]
@@ -22,20 +31,27 @@ namespace xsoverlay_font_changer.Patches
         {
             Plugin.Logger.LogInfo($"Keyboard font patcher is loaded");
 
-            XConfig.KeyboardEnable.SettingChanged += (_, _) =>
+            XConfig.KeyboardEnable.SettingChanged += (sender, args) =>
             {
                 if (IsKeyboardExist)
+                {
                     if (IsEnabled())
-                        ApplyFontPatch(KeyboardOverlay);
+                        ApplyKeyboardFont(true);
                     else
-                        RestoreFontPatch(KeyboardOverlay);
+                        RestoreKeyboardFont();
+                }
             };
 
-            XConfig.KeyboardPath.SettingChanged += (_, _) =>
+            XConfig.KeyboardPath.SettingChanged += (sender, args) =>
             {
-                if (IsKeyboardExist)
-                    if (IsEnabled())
-                        ApplyFontPatch(KeyboardOverlay);
+                if (IsKeyboardExist && IsEnabled())
+                    ApplyKeyboardFont(true);
+            };
+
+            XConfig.KeyboardScale.SettingChanged += (sender, args) =>
+            {
+                if (IsKeyboardExist && IsEnabled() && KeyboardOverlay != null)
+                    ApplyKeyboardFont(false);
             };
         }
 
@@ -53,16 +69,19 @@ namespace xsoverlay_font_changer.Patches
                 // Store original font and font size for each TextMeshProUGUI in the keyboard overlay
                 {
                     // Default keyboard
-                    foreach (TextMeshProUGUI textMesh in KeyboardOverlay.Keyboard.GetComponentsInChildren<TextMeshProUGUI>(true))
-                        Original[textMesh.GetInstanceID()] = (textMesh.font, textMesh.fontSize);
+                    foreach (KeyboardKey key in KeyboardOverlay.Keyboard.GetComponentsInChildren<KeyboardKey>(true))
+                    {
+                        TMP_Text mainText = (TMP_Text)MainText.GetValue(key);
+                        Original[key.gameObject.GetInstanceID()] = (mainText.font, key.FontSize);
+                    }
 
                     // KeyboardOSC
                     foreach (TextMeshProUGUI textMesh in GetKeyboardOscCanvas())
-                        Original[textMesh.GetInstanceID()] = (textMesh.font, textMesh.fontSize);
+                        Original[textMesh.gameObject.GetInstanceID()] = (textMesh.font, textMesh.fontSize);
                 }
 
                 if (IsEnabled())
-                    ApplyFontPatch(KeyboardOverlay);
+                    ApplyKeyboardFont(true);
 
                 IsKeyboardExist = true;
             }
@@ -70,43 +89,89 @@ namespace xsoverlay_font_changer.Patches
 
         [HarmonyPatch(typeof(KeyboardKey), "SetupKeyState")]
         [HarmonyPostfix]
-        public static void ApplyKeyFontSize(TMP_Text ___MainText)
+        public static void ListenForKeyboardKeyFontSizeChanged(KeyboardKey __instance, TMP_Text ___MainText)
         {
-            float fontSize = ___MainText.fontSize;
-
-            XConfig.KeyboardScale.SettingChanged += (_, _) =>
-            {
-                if (IsKeyboardExist && IsEnabled())
-                    ___MainText.fontSize = fontSize + XConfig.KeyboardScale.Value;
-            };
-
-            ___MainText.fontSize = fontSize + XConfig.KeyboardScale.Value;
+            if (IsEnabled())
+                ___MainText.fontSize = __instance.FontSize + XConfig.KeyboardScale.Value;
         }
 
-        private static void ApplyFontPatch(Overlay_Manager instance)
+        [HarmonyPatch(typeof(KeyboardKey), "UpdateKeyTextPreview")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> UpdateKeyTextPreviewAnimation(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            bool patched24 = false;
+            bool patched40 = false;
+
+            var getMainTextMethod = AccessTools.Method(typeof(PatchKeyboard), nameof(GetMainTextSize));
+            var getSecondaryTextMethod = AccessTools.Method(typeof(PatchKeyboard), nameof(GetSecondaryTextSize));
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_R4 && codes[i].operand != null)
+                {
+                    float val = System.Convert.ToSingle(codes[i].operand);
+
+                    if (Mathf.Approximately(val, 24f)) // Match and safely rewrite 24f
+                    {
+                        codes[i].opcode = OpCodes.Call;
+                        codes[i].operand = getMainTextMethod;
+                        patched24 = true;
+                    }
+                    else if (Mathf.Approximately(val, 40f)) // Match and safely rewrite 40f
+                    {
+                        codes[i].opcode = OpCodes.Call;
+                        codes[i].operand = getSecondaryTextMethod;
+                        patched40 = true;
+                    }
+                }
+            }
+
+            if (!patched24)
+                Plugin.Logger.LogError("[Keyboard Patch] Failed to find the 24f scale constant inside UpdateKeyTextPreview!");
+            if (!patched40)
+                Plugin.Logger.LogError("[Keyboard Patch] Failed to find the 40f scale constant inside UpdateKeyTextPreview!");
+
+            return codes;
+        }
+
+        private static void ApplyKeyboardFont(bool changeFontFace)
         {
             if (File.Exists(XConfig.KeyboardPath.Value))
             {
-                Font font = new(XConfig.KeyboardPath.Value);
-                TMP_FontAsset fontAsset = TMP_FontAsset.CreateFontAsset(font);
+                if (changeFontFace || CachedFontAsset == null)
+                    CachedFontAsset = TMP_FontAsset.CreateFontAsset(new(XConfig.KeyboardPath.Value));
 
                 // Default keyboard
-                foreach (TextMeshProUGUI textMesh in instance.Keyboard.GetComponentsInChildren<TextMeshProUGUI>(true))
+                foreach (KeyboardKey key in KeyboardOverlay.Keyboard.GetComponentsInChildren<KeyboardKey>(true))
                 {
-                    if (Original.TryGetValue(textMesh.GetInstanceID(), out (TMP_FontAsset fontAsset, float fontSize) original))
+                    if (key == null) continue;
+                    TMP_Text mainText = (TMP_Text)MainText.GetValue(key);
+
+                    if (mainText != null)
                     {
-                        textMesh.font = fontAsset;
-                        textMesh.fontSize = original.fontSize + XConfig.KeyboardScale.Value;
+                        if (mainText.font != CachedFontAsset)
+                            mainText.font = CachedFontAsset;
+
+                        mainText.fontSize = key.FontSize + XConfig.KeyboardScale.Value;
                     }
+
+                    DisplayTextFontScale.SetValue(key, 42f + XConfig.KeyboardScale.Value);
+                    CornerTextFontScale.SetValue(key, 16f + XConfig.KeyboardScale.Value);
                 }
 
                 // KeyboardOSC
                 foreach (TextMeshProUGUI textMesh in GetKeyboardOscCanvas())
                 {
-                    if (Original.TryGetValue(textMesh.GetInstanceID(), out (TMP_FontAsset fontAsset, float fontSize) original))
+                    if (textMesh == null) continue;
+
+                    if (Original.TryGetValue(textMesh.gameObject.GetInstanceID(), out (TMP_FontAsset fontAsset, float fontSize) original))
                     {
-                        textMesh.font = fontAsset;
+                        if (textMesh.font != CachedFontAsset)
+                            textMesh.font = CachedFontAsset;
+
                         textMesh.fontSize = original.fontSize + XConfig.KeyboardScale.Value;
+                        textMesh.fontSizeMax = textMesh.fontSize;
                     }
                 }
             }
@@ -114,22 +179,27 @@ namespace xsoverlay_font_changer.Patches
                 Plugin.Logger.LogError($"Keyboard - \"{XConfig.KeyboardPath.Value}\" does not exist.");
         }
 
-        private static void RestoreFontPatch(Overlay_Manager instance)
+        private static void RestoreKeyboardFont()
         {
             // Default keyboard
-            foreach (TextMeshProUGUI textMesh in instance.Keyboard.GetComponentsInChildren<TextMeshProUGUI>(true))
+            foreach (KeyboardKey key in KeyboardOverlay.Keyboard.GetComponentsInChildren<KeyboardKey>(true))
             {
-                if (Original.TryGetValue(textMesh.GetInstanceID(), out (TMP_FontAsset fontAsset, float fontSize) original))
+                if (Original.TryGetValue(key.gameObject.GetInstanceID(), out (TMP_FontAsset fontAsset, float fontSize) original))
                 {
-                    textMesh.font = original.fontAsset;
-                    textMesh.fontSize = original.fontSize;
+                    TMP_Text mainText = (TMP_Text)MainText.GetValue(key);
+
+                    mainText.font = original.fontAsset;
+                    mainText.fontSize = original.fontSize;
+
+                    DisplayTextFontScale.SetValue(key, 42f);
+                    CornerTextFontScale.SetValue(key, 16f);
                 }
             }
 
             // KeyboardOSC
             foreach (TextMeshProUGUI textMesh in GetKeyboardOscCanvas())
             {
-                if (Original.TryGetValue(textMesh.GetInstanceID(), out (TMP_FontAsset fontAsset, float fontSize) original))
+                if (Original.TryGetValue(textMesh.gameObject.GetInstanceID(), out (TMP_FontAsset fontAsset, float fontSize) original))
                 {
                     textMesh.font = original.fontAsset;
                     textMesh.fontSize = original.fontSize;
